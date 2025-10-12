b161a88952eba22e3ea3ace0294ac3ca66fe8f72
david <david@david.home>
2025-10-12 10:28:13 +0200

    modif

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 14:12:27 by david             #+#    #+#             */
-/*   Updated: 2025/10/12 09:59:49 by david            ###   ########.fr       */
+/*   Updated: 2025/10/12 10:27:36 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,7 @@ void	iter(T *tab, size_t const& size, void (*func)(T&)){
 }
 
 template<typename T>
-void	iter(const T *tab, size_t const& size, void (*func)(const T&))
-{
+void	iter(const T *tab, size_t const& size, void (*func)(const T&)){
 	for (size_t i = 0; i < size; i++)
 		func(tab[i]);
 }
